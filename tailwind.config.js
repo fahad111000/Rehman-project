21ad7146df06254434e3a00be413f8e87c446271
fahad111000 <fahad.kh2210@gmail.com>
2025-05-26 02:19:02 +0500

    Added config file and recreate navbar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+export default {
+    content: ["./src/**/*.{html,js}", "./index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+            color: {
+                primary:"#096093" // Custom color(blue)
+            }
+        },
+    },
+    plugins: [],
+}
