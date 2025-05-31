727ed6a826007086648fdb02ca7cb81f19543811
fahad111000 <fahad.kh2210@gmail.com>
2025-05-31 21:00:28 +0500

    custome breakpoint created

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,12 @@ export default {
                 montserrat: ['Montserrat', 'sans-serif'],
             },
             color: {
-                primary:"#096093" // Custom color(blue)
-            }
+                primary: "#096093" // Custom color(blue)
+            },
+            
+            screens: {
+                'md950': { 'min': '768px', 'max': '950px' },
+            },
         },
     },
     plugins: [],
