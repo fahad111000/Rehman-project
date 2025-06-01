0946e7bcc030e7e19196a674b3b5cf886f977846
fahad111000 <fahad.kh2210@gmail.com>
2025-06-01 19:36:13 +0500

    home section is fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,9 @@ export default {
             color: {
                 primary: "#096093" // Custom color(blue)
             },
-            
+
             screens: {
-                'md950': { 'min': '768px', 'max': '950px' },
+                'smlaptop': { 'max': '1390px' },
             },
         },
     },
